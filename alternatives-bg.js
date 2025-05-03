0a7f63f283c0387622ac16b34e522cf2a60261d8
Sarang Joshi <sarangj@msn.com>
2025-05-03 15:03:13 -0500

    Finish primitive prototype

--- alternatives-bg.js
+++ alternatives-bg.js
@@ -1,6 +1,11 @@
+console.log("Welcome to the Recipe Alternatives background script!");
+console.log(
+  "Here, we add a menu item to the right-click menu, and register the callback where we send a message to our main script when clicked."
+);
+
 browser.menus.create({
   id: "log-selection",
-  title: "Log deez nuts",
+  title: "Look up alternatives",
   contexts: ["selection"],
 });
 
@@ -12,18 +17,73 @@ browser.menus.onClicked.addListener((info, tab) => {
   }
 });
 
-function handleSelection(info) {
-  console.log("Looking up alternatives for: " + info.selectionText);
+async function sendMessageToBrowserScript(message) {
+  const tabs = await browser.tabs.query({ currentWindow: true, active: true });
 
-  /*
-  const selection = document.getSelection();
-  console.log(selection);
+  for (const tab of tabs) {
+    try {
+      const response = await browser.tabs.sendMessage(tab.id, message);
+      const title = response.response;
+      console.log("Document title:", title);
+      return title;
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+  }
+}
 
-  const range = selection.getRangeAt(0);
-  const clientRects = range.getClientRects();
+async function handleSelection(info) {
+  const text = info.selectionText;
 
-  console.log(clientRects);
-  */
+  console.log("Looking up alternatives for: " + text);
 
-  browser.runtime.sendMessage({ payload: "clicked" });
+  try {
+    const documentTitle = await sendMessageToBrowserScript({
+      msgType: "menuClicked",
+      payload: text,
+    });
+
+    console.log("Message sent, overlay created, now querying HuggingFace...");
+
+    await makeQuery(text, documentTitle);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+}
+
+const token = ""; /* Replace with contents of api_key.txt */
+
+async function makeQuery(query, documentTitle) {
+  console.log("Hello from makeQuery", query);
+
+  const response = await fetch("https://router.huggingface.co/novita/v3/openai/chat/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      messages: [
+        {
+          role: "user",
+          content: `What are some ideas to replace the ingredient ${query} in a recipe titled "${documentTitle}"?`,
+        },
+      ],
+      model: "deepseek/deepseek-v3-0324",
+      stream: false,
+    }),
+  });
+
+  const resJson = await response.json();
+
+  console.log(resJson);
+
+  const topResponse = resJson?.choices?.[0]?.message?.content;
+
+  const res = await sendMessageToBrowserScript({
+    msgType: "queryCompleted",
+    payload: topResponse,
+  });
+
+  console.log("Response forwarded to content script. Got ack: ", res);
 }
--- alternatives.js
+++ alternatives.js
@@ -1,9 +1,47 @@
 console.log("Welcome to Recipe Alternatives!");
 
+const OVERLAY_ID = "overlayText";
+
+// Create an overlay near where the user right-clicked
+function createOverlay(rect, text) {
+  console.log(rect);
+
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
+  overlay.id = OVERLAY_ID;
+
+  const fullText = `Hi Sowmya! Looking up alternatives for: ${text}...`;
+
+  overlay.innerText = fullText;
+
+  overlay.style.top = `${rect.bottom}px`;
+  overlay.style.left = `${rect.left}px`;
+
+  // TODO add button to close overlay
+
+  document.body.appendChild(overlay);
+}
+
 function receiver(message) {
-  console.log("received msg");
-  console.log(message);
+  console.log("received msg from bg script!");
+  console.log(message.msgType);
+
+  switch (message.msgType) {
+    case "menuClicked":
+      const selection = document.getSelection();
+      console.log(selection);
+
+      const range = selection.getRangeAt(0);
+      const clientRects = range.getClientRects();
+
+      createOverlay(clientRects[0], message.payload);
+
+      return Promise.resolve({ response: document.title });
+    case "queryCompleted":
+      document.getElementById(OVERLAY_ID).innerText = message.payload;
+      return Promise.resolve({ response: "ack" });
+  }
 }
 
 browser.runtime.onMessage.addListener(receiver);
-console.log(browser.runtime.onMessage.hasListener());
+// console.log("Has listener?", browser.runtime.onMessage.hasListener(receiver));
